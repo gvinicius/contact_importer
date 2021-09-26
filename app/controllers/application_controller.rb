8297b1706765605ef8c3ba531c8a6cfd98381ffb
Vinicius Lins <vlinsufba@gmail.com>
2021-09-26 05:07:10 -0300

    Remove email index from devise table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = %i[username password password_confirmation remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: %i[username password]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :username, uniqueness: true
+  validates :email, :uniqueness => {:allow_blank => true}
+
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
